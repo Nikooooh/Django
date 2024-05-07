c97ba48bb1b3c7e4c0185cc4d62522308dd71588
Nikooooh <nicolastrevis@hotmail.com>
2024-05-07 10:57:16 -0300

    Using Django Templates

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -12,6 +13,12 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-    
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from myapp.models import Post
+from django.views import generic 
 
-# Create your views here.
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by("-created_at")
+    template_name = "index.html"
+
+class PostDetail(generic.DetailView):
+    model = Post 
+    template_name = "post_detail.html"
